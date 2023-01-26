4555e5694b9b0fa91dfc6389807ed416a0a04654
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-01-26 19:29:16 -0300

    Correção de erros

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.alunoView, name='aluno-lista'),
+    path('', views.alunoView, name='aluno-lista'),  # O campo vazio aqui leva à lista de alunos.
     path('aluno/<int:id>', views.alunoidView, name='aluno-view'),
     path('exemplo/', views.exemplo, name='exemplo'),
     path('newaluno/', views.newAluno, name='new-aluno'),
--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('main.urls')),  # O campo vazio aqui leva à lista de alunos.
 ]
